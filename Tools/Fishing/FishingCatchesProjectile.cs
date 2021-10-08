865e9e38287484264bf70135b22d232e56ae822d
direwolf420 <amakejk1@web.de>
2021-10-08 23:21:54 +0200

    Add fishing catch drop table tool (#11)

---
+++ Tools/Fishing/FishingCatchesProjectile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ModdersToolkit.Tools.Fishing
+{
+	// This class is used in conjunction with the FishingTool to "trigger" a catch the first tick the bobber gets wet
+	// It then gets logged, and will repeat every call of AI until the testing is complete
+	internal class FishingCatchesProjectile : GlobalProjectile
+	{
+		public override void AI(Projectile projectile) {
+			if (!FishingTool.currentlyTesting) {
+				return;
+			}
+			if (!projectile.bobber) {
+				return;
+			}
+			if (!projectile.wet) {
+				return;
+			}
+
+			Player player = Main.player[projectile.owner];
+
+			//Simulate pre-FishingCheck preparation code:
+			projectile.ai[0] = 0f; // Catchable state
+			projectile.localAI[1] = 0f; // Catch timer reached
+
+			if (Main.myPlayer == projectile.owner) {
+				projectile.FishingCheck(); // Calls ModPlayer.CatchFish, after that assigns localAI[1] to the catch type
+			}
+
+			// Player.ItemCheck then checks if ai[0] is 0, and localAI[1] is above 0, then sets ai[0] to 1f and ai[1] to be localAI[1]
+			// Simulate Player.ItemCheck here:
+			if (projectile.ai[0] == 0 && projectile.localAI[1] > 0) {
+				projectile.ai[0] = 1f;
+				projectile.ai[1] = projectile.localAI[1];
+			}
+
+			// Prematurely reset it here:
+			if (projectile.ai[1] > 0f && projectile.localAI[1] >= 0f) {
+				projectile.localAI[1] = -1;
+				// ...
+			}
+
+			// Projectile.AI then checks if ai[0] >= 1f and will reel in the bobber
+			// Once it collides with the player, it will spawn the item assigned to ai[1]
+
+			// The collision is prevented to not spawn the items on the player during testing by 
+			// 1. only calling AI (and not Update, which calls HandleMovement)
+			// 2. moving the player hitbox out of the way before starting the testing
+
+			int itemType = (int)projectile.ai[1];
+
+			Dictionary<int, int> catches = FishingTool.catches;
+			catches.TryGetValue(itemType, out int currentCount);
+			catches[itemType] = currentCount + 1;
+		}
+	}
+}
---
+++ Tools/Fishing/UIFishingCatchesSlot.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.UI;
+using Terraria.DataStructures;
+
+namespace ModdersToolkit.Tools.Fishing
+{
+	// code based on SpawnUI
+	internal class UIFishingCatchesSlot : UIElement
+	{
+		public static Texture2D backgroundTexture = Main.inventoryBack9Texture;
+		private float scale = .6f;
+		public int itemType;
+		public Item item;
+
+		public UIFishingCatchesSlot(Item item) {
+			this.item = item;
+			this.itemType = item.type;
+			this.Width.Set(backgroundTexture.Width * scale, 0f);
+			this.Height.Set(backgroundTexture.Height * scale, 0f);
+		}
+
+		protected override void DrawSelf(SpriteBatch spriteBatch) {
+			//Main.instance.LoadItem(itemType);
+			Texture2D itemTexture = Main.itemTexture[itemType];
+
+			int width = itemTexture.Width;
+			int height = itemTexture.Height;
+
+			Rectangle itemDrawRectangle;
+			DrawAnimation drawAnim = Main.itemAnimations[itemType];
+			if (drawAnim != null) {
+				itemDrawRectangle = drawAnim.GetFrame(itemTexture);
+				height = itemDrawRectangle.Height;
+				width = itemDrawRectangle.Width;
+			}
+			else {
+				itemDrawRectangle = itemTexture.Bounds;
+			}
+
+			CalculatedStyle dimensions = base.GetInnerDimensions();
+			spriteBatch.Draw(backgroundTexture, dimensions.Position(), null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+			float drawScale = 2f;
+			float availableWidth = (float)backgroundTexture.Width * scale;
+			if (width * drawScale > availableWidth || height * drawScale > availableWidth) {
+				if (width > height) {
+					drawScale = availableWidth / width;
+				}
+				else {
+					drawScale = availableWidth / height;
+				}
+			}
+			Vector2 drawPosition = dimensions.Position();
+			drawPosition.X += backgroundTexture.Width * scale / 2f - (float)width * drawScale / 2f;
+			drawPosition.Y += backgroundTexture.Height * scale / 2f - (float)height * drawScale / 2f;
+
+			Color color = (item.color != new Color(byte.MinValue, byte.MinValue, byte.MinValue, byte.MinValue)) ? new Color(item.color.R, item.color.G, item.color.B, 255f) : new Color(1f, 1f, 1f);
+
+			spriteBatch.Draw(itemTexture, drawPosition, itemDrawRectangle, color, 0, Vector2.Zero, drawScale, SpriteEffects.None, 0);
+
+			if (IsMouseHovering) {
+				string name;
+				if (itemType <= 0) {
+					name = "Nothing";
+				}
+				else {
+					name = Lang.GetItemNameValue(item.type) + (item.modItem != null ? " [" + item.modItem.mod.Name + "]" : "");
+				}
+				Main.hoverItemName = name;
+			}
+		}
+
+		public override int CompareTo(object obj) {
+			UIFishingCatchesSlot other = obj as UIFishingCatchesSlot;
+			return /*-1 * */itemType.CompareTo(other.itemType);
+		}
+	}
+}
